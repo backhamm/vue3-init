2cb2ae1dff2f1e0eb5d19f27b10eeef23432c28e
edison <42608018+backhamm@users.noreply.github.com>
2023-05-29 12:50:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav><router-link to="/">Home</router-link>{{ getArr }}</nav>
+  <router-view />
+</template>
+
+<script lang="ts" setup>
+import { computed, onMounted, reactive } from "vue";
+const count = reactive<{ a: number }>({ a: 1 });
+
+onMounted(() => {
+  setTimeout(() => {
+    count.a++;
+  }, 2000);
+});
+
+const getArr = computed(() => {
+  return count.a + 1;
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
